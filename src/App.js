719d7c0adc3b676f5b3339c06b99482dd5c0b379
Edgar Benavides <edgarben27@gmail.com>
2022-04-30 12:05:14 -0500

    search bar added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const options = {
   enableHighAccuracy: true,
@@ -12,6 +14,7 @@ const App = () => {
   const [lat, setLat] = useState(0)
   const [lng, setLng] = useState(0)
   const [weather, setWeather] = useState({})
+  const [query, setQuery] = useState('')
 
   if (navigator.geolocation) {
     navigator.permissions
@@ -41,6 +44,36 @@ const App = () => {
     alert('Geolocation is not supported by this browser.')
   }
 
+  const handleSubmit = e => {
+    e.preventDefault()
+    if (query) {
+      axios
+        .get(
+          `${process.env.REACT_APP_API_BASE_URL}/current.json?key=${process.env.REACT_APP_API_KEY}&q=${query}&lang=${language}`
+        )
+        .then(res => setWeather(res.data))
+        .catch(err => {
+          const {
+            response: {
+              data: {
+                error: { message }
+              }
+            }
+          } = err
+          toast.error(message, {
+            position: 'top-left',
+            autoClose: 4000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: false,
+            progress: undefined
+          })
+        })
+      setQuery(current => '')
+    }
+  }
+
   useEffect(() => {
     setLanguage(navigator.userLanguage || navigator.language)
   }, [language])
@@ -53,22 +86,32 @@ const App = () => {
       .then(res => setWeather(res.data))
   }, [lat, lng, language])
 
-  console.log(weather, language)
-
   return (
-    <div className='border shadow-md border-gray-200 mx-10 my-10 p-5 rounded-3xl'>
-      <div className='border text-center text-3xl font-bold'>Weather App</div>
+    <div className='flex flex-col gap-3 mx-10 my-10 p-5'>
+      <div className='text-center text-3xl font-bold'>Weather App</div>
+      <form onSubmit={e => handleSubmit(e)}>
+        <input
+          type='text'
+          value={query}
+          onChange={e => setQuery(e.target.value)}
+          placeholder='Type your zip, country or city'
+          className='text-2xl w-full outline-none py-5 px-10 border rounded-full shadow-md'
+        />
+      </form>
       {weather.location && weather.current && (
-        <div className='border'>
-          <div className='border pt-10 flex flex-col justify-center items-center'>
-            <h2>{weather.current.condition.text}</h2>
+        <div className='flex flex-col gap-10'>
+          <div className='pt-10 flex flex-col justify-center items-center'>
             <img
               src={weather.current.condition.icon}
               alt={`Icon of ${weather.current.condition.text}`}
+              className='w-36'
             />
+            <h3 className='text-3xl font-bold'>
+              {weather.current.condition.text}
+            </h3>
           </div>
-          <div>
-            <ul>
+          <div className='flex gap-3 justify-between'>
+            <ul className='border shadow-md rounded-lg w-full p-5'>
               {Object.keys(weather.location).map((entry, index) => {
                 const { location } = weather
                 return (
@@ -78,9 +121,7 @@ const App = () => {
                 )
               })}
             </ul>
-          </div>
-          <div>
-            <ul>
+            <ul className='border shadow-md rounded-lg w-full p-5'>
               {Object.keys(weather.current).map((entry, index) => {
                 const { current } = weather
                 return (
@@ -95,6 +136,7 @@ const App = () => {
           </div>
         </div>
       )}
+      <ToastContainer />
     </div>
   )
 }
